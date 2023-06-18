124f61bf6f15630ff911ffbbebcfdd3e4e48d8cc
Felipe Badaro <felipe_badaro@yahoo.com.br>
2023-06-18 14:57:31 -0300

    adds 3 pages and the files for the CSS for each one

---
+++ pages/contact.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+const contact = () => {
+  return (
+    <>
+      <h1>dentro do contact</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae,
+        harum. Quisquam nihil molestiae illum. Eius possimus quos eveniet,
+        ducimus sapiente qui temporibus perspiciatis hic fugit soluta sint,
+        obcaecati inventore totam doloremque magni commodi dolores, omnis
+        eligendi in impedit adipisci unde perferendis. Natus inventore qui
+        saepe, laboriosam, sit officiis alias quam quibusdam eos odit odio
+        impedit! Nulla voluptatibus nihil eos architecto quidem ullam, et
+        tempore ad. Laudantium itaque iure earum eos, illo labore assumenda enim
+        qui esse autem, voluptate natus placeat consequuntur impedit voluptatem,
+        exercitationem molestias vero ex. Temporibus aspernatur magni neque
+        possimus praesentium ipsam numquam eaque voluptates debitis! Non, hic.
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae,
+        harum. Quisquam nihil molestiae illum. Eius possimus quos eveniet,
+        ducimus sapiente qui temporibus perspiciatis hic fugit soluta sint,
+        obcaecati inventore totam doloremque magni commodi dolores, omnis
+        eligendi in impedit adipisci unde perferendis. Natus inventore qui
+        saepe, laboriosam, sit officiis alias quam quibusdam eos odit odio
+        impedit! Nulla voluptatibus nihil eos architecto quidem ullam, et
+        tempore ad. Laudantium itaque iure earum eos, illo labore assumenda enim
+        qui esse autem, voluptate natus placeat consequuntur impedit voluptatem,
+        exercitationem molestias vero ex. Temporibus aspernatur magni neque
+        possimus praesentium ipsam numquam eaque voluptates debitis! Non, hic.
+      </p>
+    </>
+  );
+};
+
+export default contact;
