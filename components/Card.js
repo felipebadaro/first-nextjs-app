fe4c1edef307baa47bb5b2944bf43f73b2565cec
Felipe Badaro <felipe_badaro@yahoo.com.br>
2023-06-24 18:23:48 -0300

    adds the link to the card for its details

--- components/Card.js
+++ components/Card.js
@@ -1,10 +1,13 @@
 import cardStyles from "@/styles/Card.module.css";
+import Link from "next/link";
 
-function Card({ title, body }) {
+function Card({ id, title, body }) {
   return (
     <div className={cardStyles.card}>
-      <h2 className={cardStyles.cardTitle}>{title}</h2>
-      <p className={cardStyles.cardText}>{body}</p>
+      <Link href={`/card/${id}`}>
+        <h2 className={cardStyles.cardTitle}>{title}</h2>
+        <p className={cardStyles.cardText}>{body}</p>
+      </Link>
     </div>
   );
 }
